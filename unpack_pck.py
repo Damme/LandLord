69a5ae8c30cc121dd0690ff5ba1b3b262bf63bba
Damme <daniel@thewiegerts.com>
2023-04-12 17:45:10 +0200

    Rewrote unpacker

--- unpack_pck.py
+++ unpack_pck.py
@@ -1,96 +1,120 @@
 #!/usr/bin/python3.5
 
 import sys
-
-if len(sys.argv) <2:
-  print( "usage:" )
-  print("  " + sys.argv[0] + " [.PCK file]", "[Folder-prefix]" )
-  print( "  " )
-  exit()
-
 import os
 from binascii import crc32
 from struct import unpack
 
-#get unpack file
-src_file_path = sys.argv[1]
+# Convert bytearray to string
+def bytearray_to_str(data):
+    return "".join(["%c" % i for i in data]).strip("\x00")
 
-#read file length
-src_file_size = os.path.getsize(src_file_path)
-#read file
-b = bytearray(open(src_file_path, 'rb').read())
+# Convert 4 bytes to a 32-bit word
+def bytes_to_word(byte_data):
+    return unpack("<I", byte_data)[0]
 
-# crude bytearray to String converter
-def toStr(b):
-  return "".join(["%c"%i for i in b]).strip("\x00")
+# Parse file header
+def parse_header(data, data_start):
+    # Split the data using "?" as the delimiter
+    parts = data.split(b'?')
 
-# convert 4 bytes to int
-def word2int(w):
-  return unpack("<I", w)[0]
+    # Get the filename, start, and length values
+    filename = bytearray_to_str(parts[0]).replace("\\", "/")
+    start = int(parts[1], 16) + data_start + 4
 
-# crude filetable parser
-def getHeader(b, datastart):
-  beginstart=b.find(63) #"?"
-  filename = toStr(b[0:beginstart]).replace("\\","/")
-  beginstart+=1
-  beginlength=b.find(63,beginstart) #"?"
-  start = int(b[beginstart:beginlength],16) + datastart + 4
-  beginlength+=1
-  length = int(b[beginlength:beginlength+10],16)
-  return {
-    "filename": filename,
-    "start" : start,
-    "length" : length,
+    # If there are 4 parts, ignore the middle part
+    special = 0
+    if len(parts) == 4:
+        length = int(parts[3], 16)
+        special = int(parts[2], 16)
+    else:
+        length = int(parts[2], 16)
+
+    return {
+        "filename": filename,
+        "start": start,
+        "length": length,
+        "special": special,
     }
 
-#crude way to create nonexisting directories
-def checkCreateDir(filename):
-  if not os.path.exists(os.path.dirname(filename)):
-    try:
-        os.makedirs(os.path.dirname(filename))
-    except OSError as exc: # Guard against race condition
-        if exc.errno != errno.EEXIST:
-            raise
 
-def save(filename, data):
-  checkCreateDir(prefix + "/" + filename) #create missing directories
-  print ("writing %s\t (%d bytes)"%(filename,len(data)))
-  open(prefix + "/" + filename, 'wb').write(data)
+# Ensure directory exists
+def ensure_dir_exists(filename):
+    directory = os.path.dirname(filename)
+    if not os.path.exists(directory):
+        try:
+            os.makedirs(directory)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
 
-## Start decoding
-print("Unknown byte 0:\t"+ str(b[0]))
-print("Unknown byte 1:\t"+ str(b[1]))
-print("Unknown byte 2:\t"+ str(b[2]))
-print("Unknown byte 3:\t"+ str(b[3]))
+# Save file
+def save_file(filename, prefix, data):
+    ensure_dir_exists(prefix + "/" + filename)
+    print(f"writing {filename}\t ({len(data)} bytes)")
+    open(prefix + "/" + filename, 'wb').write(data)
 
-prefix = sys.argv[2] if len(sys.argv)>=3 else "."
+def main():
+    # Check command line arguments
+    if len(sys.argv) < 2:
+        print("Usage:")
+        print(f"  {sys.argv[0]} [.PCK file] [Folder-prefix]")
+        print()
+        exit()
 
-headerstart_token = bytearray((int(62), int(0x0d), int(0x0a)))
-headerstart = b.find(headerstart_token,0)+3
-pos=headerstart
-datastart_token = bytearray((int(0x0d), int(0x0a), int(0x0d), int(0x0a)))
-datastart = b.find(datastart_token,0)
-print("header end: \t"+ str(datastart))
-nHeaders = b.count(int(13),4,datastart)
-print("# files \t: "+str(nHeaders))
+    # File to unpack
+    src_file_path = sys.argv[1]
+    # File size
+    src_file_size = os.path.getsize(src_file_path)
+    # Read file into bytearray
+    binary_data = bytearray(open(src_file_path, 'rb').read())
+    
+    
+    # Start decoding
+    #print(f"Unknown byte 0:\t{binary_data[0]}")
+    #print(f"Unknown byte 1:\t{binary_data[1]}")
+    #print(f"Unknown byte 2:\t{binary_data[2]}")
+    #print(f"Unknown byte 3:\t{binary_data[3]}")
+    print(f"Unknown first 32-bit value: {hex(bytes_to_word(binary_data[0:4]))}")
+    
+    if binary_data[4] != int(0x3c):
+        #print(f"Unknown byte 4:\t{binary_data[4]}")
+        #print(f"Unknown byte 5:\t{binary_data[5]}")
+        #print(f"Unknown byte 6:\t{binary_data[6]}")
+        #print(f"Unknown byte 7:\t{binary_data[7]}")
+        print(f"Unknown second 32-bit value: {hex(bytes_to_word(binary_data[4:8]))}")
+    
+    print(f"Unknown last 32-bit value: {hex(bytes_to_word(binary_data[-4:]))}")
 
+    prefix = sys.argv[2] if len(sys.argv) >= 3 else "."
 
-# crude way to parse the directory table
-files = []
-print ("found %i files\n"%nHeaders)
+    header_start_token = bytearray((int(0x3e), int(0x0d), int(0x0a)))
+    header_start = binary_data.find(header_start_token, 0) + 3
+    pos = header_start
+    data_start_token = bytearray((int(0x0d), int(0x0a), int(0x0d), int(0x0a)))
+    data_start = binary_data.find(data_start_token, 0)
+    print(f"header end: \t{data_start}")
+    num_files = binary_data.count(int(13), 4, data_start)
+    print(f"# files \t: {num_files}")
 
-token = bytearray((int(0x0d), int(0x0a)))
+    # Parse directory table
+    files = []
+    print(f"found {num_files} files\n")
 
-for i in range(nHeaders):
-  pos2=b.find(token,pos)
-  h = getHeader(b[pos:pos2], datastart)
-  files.append(h)
-  print(h)
-  pos = int(pos2)+2
+    token = bytearray((int(0x0d), int(0x0a)))
 
-for h in files:
-  bFile = b[h["start"]:h["start"]+h["length"]]
-#write stripped file
-  save(h["filename"],bFile)
+    for _ in range(num_files):
+        pos2 = binary_data.find(token, pos)
+        header = parse_header(binary_data[pos:pos2], data_start)
+        files.append(header)
+        print(header)
+        pos = int(pos2) + 2
 
-print("Done!")
+    for header in files:
+        file_data = binary_data[header["start"]:header["start"] + header["length"]]
+        save_file(header["filename"], prefix, file_data)
+    
+    print("Done!")
+
+if __name__ == '__main__':
+    main()
